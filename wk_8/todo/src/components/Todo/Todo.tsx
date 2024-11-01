c9499ed89735b396082eba945885171dc1c9e708
Christian Lam <z5359842@ad.unsw.edu.au>
2024-11-01 11:51:05 +1100

    feat: Template for todolist

---
+++ wk_8/todo/src/components/Todo/Todo.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './Todo.css';
+
+export default function Todo() {
+  return (
+    <div className='todo'>
+      <div>
+        <h3>Todo</h3>
+        <p>Description</p>
+      </div>
+      <input type='checkbox' className='checkbox' />
+    </div>
+  );
+}
